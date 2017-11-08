cfcf1923814a88c2ac261d8df701f019bd382fff
Mingzhang <redondoself@gmail.com>
2017-11-08 16:31:18 -0500

    server set

---
+++ web/backend/app/http/sampleData.js
@@ -0,0 +1,72 @@
+/**
+ * Created by mingzhang on 11/7/17.
+ */
+'use strict';
+
+let data = [
+  {
+    'name':'MIR1302-2',
+    'gene_id':100302278,
+    'start':30366,
+    'end':30503,
+    'chr':'NC_000001.11',
+    'TPM1':1.23,
+    'TPM2':6.34,
+    'PValue':1.03E-15,
+    'BAM1':'MIR1302-2_1_sorted.bam',
+    'BAM2':'MIR1302-2_2_sorted.bam'
+  },
+  {
+    'name':'FAM138A',
+    'gene_id':645520,
+    'start':34611,
+    'end':36081,
+    'chr':'NC_000001.11',
+    'TPM1':7.34,
+    'TPM2':0.01,
+    'PValue':3.45E-15,
+    'BAM1':'FAM138A_1_sorted.bam',
+    'BAM2':'FAM138A_2_sorted.bam'
+  },
+  {
+    'name':'LOC102723897',
+    'gene_id':102723897,
+    'start':184878,
+    'end':199860,
+    'chr':'',
+    'TPM1':4.86,
+    'TPM2':3.21,
+    'PValue':0.123,
+    'BAM1':'LOC102723897_1_sorted.bam',
+    'BAM2':'LOC102723897_2_sorted.bam'
+  },
+  {
+    'name':'CICP7',
+    'gene_id':100288667,
+    'start':487055,
+    'end':490819,
+    'chr':'NC_000001.11',
+    'TPM1':22.12,
+    'TPM2':8.56,
+    'PValue':1.67E-16,
+    'BAM1':'CICP7_1_sorted.bam',
+    'BAM2':'CICP7_2_sorted.bam'
+  },{
+    'name':'VTCN1',
+    'gene_id':79679,
+    'start':117143587,
+    'end':117210992,
+    'chr':'NC_000001.11',
+    'TPM1':3.45,
+    'TPM2':4.12,
+    'PValue':0.002,
+    'BAM1':'VTCN1_1_sorted.bam',
+    'BAM2':'VTCN1_2_sorted.bam'
+  }
+];
+
+if (typeof module !== 'undefined' && module.parent) {
+  //module.exports =
+} else {
+
+} 
