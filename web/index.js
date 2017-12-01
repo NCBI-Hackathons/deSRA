2143d8b2a0580c76ff8799a1ce51def5134f1bff
Mingzhang Yang <redondoself@gmail.com>
2017-12-01 16:39:40 -0500

    backend development ongoin

--- web/index.js
+++ web/index.js
@@ -11,13 +11,14 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var app = express();
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+
 app.use('/', express.static('public'));
 
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
 // error handler
 app.use(function(err, req, res, next) {
