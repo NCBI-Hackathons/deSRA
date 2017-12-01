1a55fe81ab2bf004ca93e69f400a37d4a4b98e18
Mingzhang Yang <redondoself@gmail.com>
2017-12-01 16:32:42 -0500

    reinstall modules

--- web/index.js
+++ web/index.js
@@ -13,6 +13,23 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use('/', express.static('public'));
 
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 app.listen('8000', () => {
   console.log('Listening on port 8000');
 });
